1136e8d94c2e77b0f9a8e99b486b44a003030950
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2023-06-04 19:04:06 +0200

    Fix errors, reorganize and simplify code
    Simplify and remove obsolete code.
    Fix bug in which operators would stack if they were selected more than
    once in a row. Now they will not stack.
    Fix error where clicking '=' more than once in a row would continue to
    calculate using last operator.
    Change parseInt() to parseFloat() so result can be a decimal value.

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-#JavaScript Calculator
+# JavaScript Calculator
 
 This is a JavaScript calculator that allows users to perform basic arithmetic operations. It provides a user-friendly interface where users can input numbers and operators to perform calculations.
 
-##Features
+## Features
 Addition, subtraction, multiplication, and division operations.
 Clear button to reset the calculator.
 Calculation of expressions involving multiple operators.
 Display of intermediate results during calculations.
 
-##Usage
+## Usage
 Use the calculator interface to input numbers and operators.
 The display area shows the current expression.
 Press the "=" button to calculate and display the result.
--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 let number1;
 let number2;
 let operator;
-let checkIfCalculated = false;
 let result;
+let firstNumber = true;
 let display = document.querySelector('.display');
 let expression = document.querySelector('.expression');
 
@@ -28,21 +28,9 @@ function operate(operator, number1, number2) {
 function clear(currentInput) {
     if (currentInput === 'C') {
         number1 = undefined;
-        checkIfCalculated = false;
+        firstNumber = true;
         display.textContent = '';
         expression.textContent = '';
-        return true;
-    }
-}
-
-function firstInputtedNumber(currentInput, operator, operators) {
-    if (number1 === undefined && operators.includes(currentInput)) {
-        number1 = parseInt(display.textContent);
-        console.log(number1);
-        display.textContent += ' ' + currentInput;
-        operator = currentInput;
-        return true;
-    } else if (display.textContent === '0' && currentInput === '0') {
         return true;
     }
 }
@@ -52,44 +40,42 @@ function displayInput() {
     const lastCharacter = display.textContent.charAt(display.textContent.length - 1);
     const operators = ['+', '-', '*', '/'];
 
-    // Clear display
-    if (clear(currentInput)) {
+    // If input is 'C'
+    // If display is showing result of calculation and '=' is selected again
+    // If display is empty and operator is selected
+    // If last character and current input are both operators
+    if (clear(currentInput)
+        || (expression.textContent === '' && currentInput === '=')
+        || (display.textContent === '' && operators.includes(currentInput))
+        || (operators.includes(currentInput) && operators.includes(lastCharacter))) {
         return;
-    }
-
-    // Return if last character and current input are both operators so operators don't stack
-    if (operators.includes(lastCharacter) && operators.includes(currentInput)) {
-        return;
-    }
-
-    // If last character is operator save it as operator
-    if (operators.includes(lastCharacter)) {
+    } else if (operators.includes(lastCharacter)) {
         operator = lastCharacter;
     }
 
-    // Check for first inputted number
-    if (firstInputtedNumber(currentInput, operator, operators)) {
-        return;
-    }
-    
-    // After first number has been inputted
-    if (operators.includes(currentInput) || currentInput == '=') {
-        if (checkIfCalculated === true) {
-            number1 = parseInt(display.textContent);
-            checkIfCalculated = false;
+    // If operator or '=' is selected then
+    if (operators.includes(currentInput) || currentInput === '=') {
+        if (firstNumber === true) {
+            number1 = parseFloat(display.textContent);
+            firstNumber = false;
             display.textContent += ' ' + currentInput;
             return;
         } else {
-            number2 = parseInt(display.textContent);
+            number2 = parseFloat(display.textContent);
         }
-        result = operate(operator, number1, number2);
-        number1 = result;
-        if (currentInput == '=') {
-            checkIfCalculated = true;
+
+        if (number2 !== undefined) {
+            result = operate(operator, number1, number2);
+            number1 = result;
+        }
+        
+        if (currentInput === '=') {
+            firstNumber = true;
             expression.textContent = '';
             display.textContent = result;
             return;
         }
+
         display.textContent += ' ' + currentInput;
         return;
     }
@@ -99,7 +85,7 @@ function displayInput() {
     if (display.textContent === '0' || operators.includes(lastCharacter)) {
         expression.textContent = display.textContent;
         display.textContent = currentInput;
-    } else if (!operators.includes(currentInput) && !operators.includes(lastCharacter)) {
+    } else if (!(operators.includes(currentInput) && operators.includes(lastCharacter))) {
         display.textContent +=  '' + currentInput;
     }
 }
